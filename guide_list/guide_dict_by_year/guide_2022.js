a026c1d9dd06340393f9438ac369a50617cd5864
Shao-Hung Yu <hank199599@gmail.com>
2022-04-22 16:19:59 +0800

    update guider list

--- guide_list/guide_dict_by_year/guide_2022.js
+++ guide_list/guide_dict_by_year/guide_2022.js
@@ -3,12 +3,53 @@ var guide_dict_2022=[
         "book_title":"精通 Python：運用簡單的套件進行現代運算（第二版）",
         "chapters":10,
         "guider_list":[
+            {
+                "pic":"../assets/img/guide_person/IIN25T90.jpg",
+                "name":"游紹宏",
+                "details":"台科大資工所 研究生"
+            },
+            {
+                "name":"張閔勛",
+                "details":"東華大學資工系 學生"
+            },
+            {
+                "name":"馬皓宸",
+                "details":"台科大資工所 研究生"
+            },
+            {
+                "name":"柯美任",
+                "details":"台科大資工所 研究生"
+            },
+            {
+                "name":"Nishkarsh arya",
+                "details":"Developer"    
+            },
+            {
+                "name":"洪譽承",
+                "details":"台灣大學電信所 研究生"
+            },
+            {
+                "name":"Robert Chen"
+            },
         ]
     },
     {
-        "book_title":"TensorFlow 資源分享",
-        "chapters":0,
+        "book_title":"TensorFlow官方API",
+        "chapters":21,
         "guider_list":[
+            {
+                "name":"廖健傑Jack"
+            },
+        ]
+    },
+    {
+        "book_title":"TensorFlow Hub 模型",
+        "chapters":9,
+        "guider_list":[
+            {
+                "name":"Jerry"
+            }
         ]
     }
+    
 ]
