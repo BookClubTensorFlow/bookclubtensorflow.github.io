99db0468f2625a2f3c3936689f12f255e1d4a87a
Hank Yu <hank.yu@choco.media>
2023-01-29 20:12:26 +0800

    feat: add verified hint in page

--- unsubscription/formControl.js
+++ unsubscription/formControl.js
@@ -10,11 +10,13 @@ function validate(e) {
   
   const email = document.getElementById("email");
   const emailInputField = document.getElementById("emailInputField");
+  const emailVailfiedHint = document.getElementById("emailVailfiedHint");
   const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   
 
   console.log(email.value)
-  console.log(emailRegex.test(email.value))
+  console.log(email.value.indexOf('@')!==-1)
+
   document.getElementById("requestEmail").innerHTML = email.value;
 
   
@@ -23,11 +25,24 @@ function validate(e) {
     emailInputField.classList.add("has-success");
     email.classList.remove("is-invalid");
     emailInputField.classList.remove("has-danger");
+ 
+    emailVailfiedHint.style.color = '#2dce89';
+    emailVailfiedHint.innerHTML = '<span class="spinner-border text-success spinner-border-sm" role="status" aria-hidden="true"></span> 驗證成功，正在提交您的請求';
+
+    
     console.log("Valid email address");
     
     SaveData()
     .then((status) => {
         console.log("data", e)
+        email.classList.remove("is-valid");
+        emailInputField.classList.remove("has-success");
+        email.classList.remove("is-invalid");
+        emailInputField.classList.remove("has-danger");
+        console.log("Valid email address");
+
+        emailVailfiedHint.innerHTML = '';
+
         if (status.result==='success'){
           let Modal = new bootstrap.Modal(document.getElementById('modal-notification-success'), {});
           Modal.show();
@@ -45,6 +60,14 @@ function validate(e) {
     email.classList.add("is-invalid");
     emailInputField.classList.add("has-danger");
 
+    emailVailfiedHint.style.color = '#fb6340';
+
+    if (email.value.indexOf('@')===-1){
+      emailVailfiedHint.innerHTML = `請在電子郵件地址中包含「＠」，「${email.value}」未包含「＠」`;
+    }else{
+      emailVailfiedHint.innerHTML = '請符合要求的格式';
+    }
+    
     console.log("Invalid email address");
   }
   
